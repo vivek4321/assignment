aa81a0a6fe191323a4f2507a0c73739b91a14fee
vivek <vivekvivek9866@gmail.com>
2019-10-28 01:31:23 -0500

    lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-
-
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { DataServiceService } from './data-service.service';
-import { ScrollDispatchModule } from '@angular/cdk/scrolling';
-import { ChartsModule } from 'ng2-charts';
-import { BarChartComponent } from './bar-chart/bar-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeComponent,
-    BarChartComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    ScrollDispatchModule,
-    ChartsModule
   ],
   providers: [DataServiceService],
   bootstrap: [AppComponent]
---
+++ src/app/lazy-home/lazy-home.module.ts
@@ -0,0 +1,24 @@
+import { BarChartComponent } from './../bar-chart/bar-chart.component';
+import { HomeComponent } from './../home/home.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LazyHomeRoutingModule } from './lazy-home-routing.module';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ScrollDispatchModule } from '@angular/cdk/scrolling';
+import { ChartsModule } from 'ng2-charts';
+
+
+@NgModule({
+  declarations: [HomeComponent, BarChartComponent],
+  imports: [
+    CommonModule,
+    LazyHomeRoutingModule,
+    ScrollDispatchModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ChartsModule
+    
+  ]
+})
+export class LazyHomeModule { }
